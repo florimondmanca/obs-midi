fbc3185715f2869a1af24ed3b4aa8d39375ab7f1
florimondmanca <florimond.manca@protonmail.com>
2025-12-26 21:04:27 +0100

    Add Help menu

--- obs_midi/gui/menu.py
+++ obs_midi/gui/menu.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import webbrowser
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -12,6 +13,7 @@ class Menu:
 
         file_menu = tk.Menu(menu, tearoff=0)
         menu.add_cascade(label="File", menu=file_menu)
+        file_menu.add_separator()
         file_menu.add_command(
             label="Quit",
             command=lambda: gui.destroy(),
@@ -28,6 +30,26 @@ class Menu:
             underline=5,
         )
         self._tools_menu = tools_menu
+
+        help_menu = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(
+            label="OBS - Remote Control Guide",
+            command=lambda: webbrowser.open(
+                "https://obsproject.com/kb/remote-control-guide"
+            ),
+        )
+        help_menu.add_separator()
+        help_menu.add_command(
+            label="Source code / Documentation",
+            command=lambda: webbrowser.open(
+                "https://codeberg.org/florimondmanca/obs-midi"
+            ),
+        )
+        help_menu.add_command(
+            label="License: MIT",
+            command=lambda: None,
+        )
 
     def set_open_midi_debug_modal_state(self, state: str) -> None:
         self._tools_menu.entryconfig("Open MIDI debug window", state=state)
